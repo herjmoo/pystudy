8d1e587397afa30acafd61e2fa3410fde4de6d94
john.lee2@nike.com <john.lee2@nike.com>
2018-07-11 23:01:14 -0700

    creating new test file

--- game/johnny_gameTest.py
+++ game/johnny_gameTest.py
@@ -1,1 +1,16 @@
-print("안녕하세요")
+class Calculator:
+    def __init__(self, a):
+        self.numberList = a
+    def sum(self):
+        self.result = 0
+        for numbers in self.numberList:
+            self.result = self.result + numbers
+        return self.result
+    def avg(self):
+        total = self.sum()
+        print(self.result)
+        return total / len(self.numberList)
+
+cal1 = Calculator([1,2,3,4,6])
+print(cal1.avg())
+print(cal1.sum())
